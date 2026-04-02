358fe83a55c59b0d49186f37089c1446e11b139d
Camilly Duarte <tecnologirlinformatica@gmail.com>
2026-04-02 02:47:19 -0300

    middleware tipos

--- express/node-express/server.js
+++ express/node-express/server.js
@@ -1,40 +1,13 @@
-const express = require('express');
-
-const app = express();
-const PORT = 3000;
-
-app.get('/', (req, res) => {
-//   res.send('Servidor Express rodando 🚀');
-res.json({message: "Olá, Mundo!"});
+// Middleware para verificar acesso ao perfil
+const checkAccess = (req, res, next) => {
+        const authorized = true; // Altere para `false` para simular um usuário não autorizado
+        if (authorized) {
+          next(); // Usuário autorizado, passa para o próximo manipulador
+        } else {
+          res.status(403).json({ message: "Acesso ao perfil negado" }); // Resposta em JSON para não autorizado
+        }
+};
+// Rota /perfil com middleware específico
+app.get("/perfil", checkAccess, (req, res) => {
+        res.json({ message: "Bem-vindo ao seu perfil!" }); // Resposta em JSON para autorizado
 });
-
-app.get("/produtos",(req, res) => {
-  res.json({message:"Listando todos os produtos", produtos: [] }); //exemplo com um array vazio de produtos
-});
-//ROTA POST para criar um novo produto
-app.post("/produtos", (req, res) => {
-  const { nome } = req.body;
-  res.json({
-    message: "Criando um novo produto",
-    produto: {id: Date.now(), nome }
-  // produto: { id:Date.now(), nome: "Produto Exemplo"}, 
-  });
-});
-//ROTA PUT para atualizar um produto por ID
-app.put("/produtos/:id", (req,res) => {
-  const { id } = req.params;
-  res.json({
-    message: "Atualizando o produto",
-  produto: { id: id, nome: "Produto Atualizado"},
-  });
-  });
-  
-  //ROTA DELETE para excluir um produto por ID
-  app.delete("/produtos/:id", (req, res) => {
-    const { id } = req.params
-    res.json({ message: "Excluindo o produto", produtoId: id });
-  });
-  //Inicia o servidor e faz com que ele escute na porta definida
-  app.listen(PORT,() => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
-  });
