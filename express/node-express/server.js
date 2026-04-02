a36c0a291fc46f318b1efdcd135cf7797e36183f
Camilly Duarte <tecnologirlinformatica@gmail.com>
2026-04-02 01:14:11 -0300

    fix att 2

--- express/node-express/server.js
+++ express/node-express/server.js
@@ -8,6 +8,33 @@ app.get('/', (req, res) => {
 res.json({message: "Olá, Mundo!"});
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+app.get("/produtos",(req, res) => {
+  res.json({message:"Listando todos os produtos", produtos: [] }); //exemplo com um array vazio de produtos
 });
+//ROTA POST para criar um novo produto
+app.post("/produtos", (req, res) => {
+  const { nome } = req.body;
+  res.json({
+    message: "Criando um novo produto",
+    produto: {id: Date.now(), nome }
+  // produto: { id:Date.now(), nome: "Produto Exemplo"}, 
+  });
+});
+//ROTA PUT para atualizar um produto por ID
+app.put("/produtos/:id", (req,res) => {
+  const { id } = req.params;
+  res.json({
+    message: "Atualizando o produto",
+  produto: { id: id, nome: "Produto Atualizado"},
+  });
+  });
+  
+  //ROTA DELETE para excluir um produto por ID
+  app.delete("/produtos/:id", (req, res) => {
+    const { id } = req.params
+    res.json({ message: "Excluindo o produto", produtoId: id });
+  });
+  //Inicia o servidor e faz com que ele escute na porta definida
+  app.listen(PORT,() => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  });
